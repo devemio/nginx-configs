d465a2ec5c18a64a9252ea54ddb236fbfce63575
Sergey Sorokin <s.sorokin@progforce.com>
2019-04-18 22:59:42 +0300

    Answer yes for apt install

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt install apt-transport-https ca-certificates
+sudo apt -y install apt-transport-https ca-certificates
 
 sudo sh -c "echo 'deb [arch=amd64] https://download.docker.com/linux/ubuntu '$(lsb_release -cs)' stable' > /etc/apt/sources.list.d/docker.list"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt update
-sudo apt install docker-ce
+sudo apt -y install docker-ce
